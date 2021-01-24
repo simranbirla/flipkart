ebb1aceea25b39e64a380e44586b8cf7e512adbf
simranbirla <62006675+simranbirla@users.noreply.github.com>
2021-01-24 13:12:13 +0530

    styling and header done

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import data from "../data.json";
 import Products from "./Products";
 import Filters from "./Filters";
+import Header from "./Header";
 import "../Style/index.css";
 const App = () => {
   const [list, setList] = useState(data);
 
   return (
-    <div className="app">
-      <Filters setList={setList} list={list} />
-      <Products list={list} />
+    <div className="app-main">
+      <Header />
+      <div className="app">
+        <Filters setList={setList} list={list} />
+        <Products list={list} />
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "../Style/Header.css";
 const Header = () => {
-  return <div>hello</div>;
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src="https://img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+          width="75px"
+          alt="logo for flipkart"
+        />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="header_right">
+        <p className="header_right-login">Login</p>
+        <p>
+          <i className="fas fa-shopping-cart"></i> Cart
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
